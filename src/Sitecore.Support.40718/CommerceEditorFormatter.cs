adef079a0245c9effd23840208242675dc3c8511
sc-AlexanderNg <alex@sitecore.net>
2018-08-29 13:52:53 +0800

    Create custom CommerceEditorFormatter which inherits from TranslatorFormatter and Modify SetCommerceEditorFormatter processor to have the flexibility in selecting not only CommerceEditorFormatter (which I customized) but also the default EditorFormatter

---
+++ src/Sitecore.Support.40718/CommerceEditorFormatter.cs
@@ -0,0 +1,104 @@
+﻿using Sitecore.Commerce.Engine.Connect.DataProvider.Pipelines.ContentEditor;
+namespace Sitecore.Support.Commerce.Engine.Connect.DataProvider.Pipelines.ContentEditor
+{
+  using System.Collections.Generic;
+  using System.Web.UI;
+  using Sitecore.Commerce.Engine.Connect.DataProvider.Definitions;
+  using Sitecore.Commerce.Engine.Connect.DataProvider;
+  using Sitecore.Data;
+  using Sitecore.Diagnostics;
+  using Sitecore.Shell.Applications.ContentEditor;
+
+  /// <summary>
+  /// Customized EditorFormatter for Commerce content items.
+  /// </summary>
+  public class CommerceEditorFormatter : TranslatorFormatter // ALEX: Changed to use TranslatorFormatter
+  {
+    /// <summary>
+    /// Renders a field.
+    /// </summary>
+    /// <param name="parent">The parent control.</param>
+    /// <param name="field">The editor field.</param>
+    /// <param name="readOnly">Specifies whether the field should be rendered as read-only.</param>
+    public override void RenderField(
+        Control parent,
+        Shell.Applications.ContentManager.Editor.Field field,
+        bool readOnly)
+    {
+      base.RenderField(
+          parent,
+          field,
+          readOnly || ShouldMakeReadOnly(field));
+    }
+
+    /// <summary>
+    /// Renders a field.
+    /// </summary>
+    /// <param name="parent">The parent control.</param>
+    /// <param name="field">The editor field.</param>
+    /// <param name="fieldType">The field type.</param>
+    /// <param name="readOnly">Specifies whether the field should be rendered as read-only.</param>
+    public new void RenderField(
+        Control parent,
+        Sitecore.Shell.Applications.ContentManager.Editor.Field field,
+        Sitecore.Data.Items.Item fieldType,
+        bool readOnly)
+    {
+      base.RenderField(
+          parent,
+          field,
+          fieldType,
+          readOnly || ShouldMakeReadOnly(field));
+    }
+
+    /// <summary>
+    /// Renders a field.
+    /// </summary>
+    /// <param name="parent">The parent control.</param>
+    /// <param name="field">The editor field.</param>
+    /// <param name="fieldType">The field type.</param>
+    /// <param name="readOnly">Specifies whether the field should be rendered as read-only.</param>
+    /// <param name="value">The value.</param>
+    public override void RenderField(
+        Control parent,
+        Sitecore.Shell.Applications.ContentManager.Editor.Field field,
+        Sitecore.Data.Items.Item fieldType,
+        bool readOnly,
+        string value)
+    {
+      base.RenderField(
+          parent,
+          field,
+          fieldType,
+          readOnly || ShouldMakeReadOnly(field),
+          value);
+    }
+
+    /// <summary>
+    /// Determines if a field should be rendered as read-only.
+    /// </summary>
+    /// <param name="field">The editor field.</param>
+    /// <returns>True if the field should be rendered as read-only, otherwise false.</returns>
+    protected virtual bool ShouldMakeReadOnly(Shell.Applications.ContentManager.Editor.Field field)
+    {
+      Assert.ArgumentNotNull(field, "field");
+      Assert.IsNotNull(Context.ContentDatabase, "Sitecore.Context.ContentDatabase");
+
+      var catalogTemplates =
+          new List<ID>
+          {
+                    KnownItemIds.CatalogGeneratedFolder,
+                    KnownItemIds.CatalogTemplateId,
+                    KnownItemIds.CategoryTemplateId,
+                    KnownItemIds.SellableItemTemplateId,
+                    KnownItemIds.SellableItemVariantTemplateId
+          };
+
+      var repository = new CatalogRepository();
+
+      return
+          (catalogTemplates.Contains(field.ItemField.Item.TemplateID) || repository.TemplateExistsInMappings(field.ItemField.Item.TemplateID)) &&
+          field.ItemField.InnerItem.Appearance.ReadOnly;
+    }
+  }
+}
---
+++ src/Sitecore.Support.40718/SetCommerceEditorFormatter.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Support.Commerce.Engine.Connect.DataProvider.Pipelines.ContentEditor;
+using Sitecore.Shell.Applications.ContentEditor;
+
+namespace Sitecore.Support.Commerce.Engine.Connect.DataProvider.Pipelines
+{
+  //using Sitecore.Commerce.Engine.Connect.DataProvider.Pipelines.ContentEditor;
+  using Sitecore.Diagnostics;
+
+  /// <summary>
+  /// Content editor pipeline that sets the <see cref="CommerceEditorFormatter"/> as the SC editor formatter.
+  /// </summary>
+  public class SetCommerceEditorFormatter
+  {
+    /// <summary>
+    /// Processes the pipeline.
+    /// </summary>
+    /// <param name="args">The pipeline arguments.</param>
+    public void Process(Sitecore.Shell.Applications.ContentEditor.Pipelines.RenderContentEditor.RenderContentEditorArgs args)
+    {
+      // As there are mutliple types of editors in Sitecore, we need to override each one and make sure we create an appropriate
+      // override compared to the type that is already set.
+      Assert.IsNotNull(args.EditorFormatter, "args.EditorFormatter");
+
+      if (args.EditorFormatter.GetType().FullName == "Sitecore.Shell.Applications.ContentEditor.TranslatorFormatter")
+      { // ALEX: when clicking "Translate" button
+        args.EditorFormatter = new CommerceEditorFormatter { Arguments = args };
+      }
+      else
+      { // ALEX: when un-clicking "Translate" button
+        args.EditorFormatter = new EditorFormatter { Arguments = args };
+      }
+    }
+  }
+}
